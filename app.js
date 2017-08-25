c5e9b1b4f061e6b493d763d70deda4c7a372d556
qus1225 <qus1225@gmail.com>
2017-08-25 14:47:28 +0900

    Set Default Express Setting

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+
+var app =  express();
+
+// Set Static Middleware
+app.use(express.static(__dirname + '/public'));
+
+app.use(require('body-parser').urlencoded({ extended: true }));
+
+app.set('port', process.env.PORT || 8080);
+
+// Set Handlebar ViewEngine
+var handlebars = require('express-handlebars')
+  .create({
+    defaultLayout: 'main',
+    helpers: {
+      section: function (name, options) {
+        if(!this._sections) this._sections = {};
+        this._sections[name] = options.fn(this);
+        return null;
+      }
+    }
+  });
+app.engine('hbs', handlebars.engine);
+app.set('view engine', 'hbs');
+
+// Routes
+app
+  .get('/', function (req, res) {
+    res.render('home');
+  })
+;
+
+// custom 404 page
+app.use(function (req, res) {
+  res.status(404);
+  res.render('404');
+});
+
+// custom 500 page
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate');
+});
